769371a3ee48c637e23e9b86a251f5c819c51c66
UmVitorAleatorio <vitorhiper@hotmail.com>
2025-07-09 11:04:03 -0300

    Projeto aprovado no BeeCrowd, uso de métodos para abstração e legibilidade do código. Criado os métodos para popular a array, e contar quantos estão dentro e fora do intervalo

--- 1072/Intervalo 2/src/main/java/Main.java
+++ 1072/Intervalo 2/src/main/java/Main.java
@@ -1,12 +1,33 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         int quantidade = leitura();
+        int[] numeros = new int[quantidade];
+        injetor(quantidade, numeros);
+
+        System.out.println(dentro(numeros) + " in");
+        System.out.println(fora(numeros) + " out");
     }
 
     static Scanner sc = new Scanner(System.in);
+
     public static int leitura() {
         return sc.nextInt();
+    }
+
+    public static void injetor(int quantidade, int[] numeros) {
+        for (int i = 0; i < quantidade; i++) {
+            numeros[i] = leitura();
+        }
+    }
+
+    public static int dentro(int[] numeros) {
+        return Arrays.stream(numeros).filter(n -> n >= 10 && n <= 20).toArray().length;
+    }
+
+    public static int fora(int[] numeros) {
+        return Arrays.stream(numeros).filter(n -> n < 10 || n > 20).toArray().length;
     }
 }
