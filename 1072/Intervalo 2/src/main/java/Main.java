26617e6c7793768e45960557ad424021fe30eaf0
UmVitorAleatorio <vitorhiper@hotmail.com>
2025-07-09 10:19:41 -0300

    Método de leitura criado para receber a quantidade e os valores determinantes

--- 1072/Intervalo 2/src/main/java/Main.java
+++ 1072/Intervalo 2/src/main/java/Main.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        int quantidade = leitura();
+    }
 
+    static Scanner sc = new Scanner(System.in);
+    public static int leitura() {
+        return sc.nextInt();
     }
 }
