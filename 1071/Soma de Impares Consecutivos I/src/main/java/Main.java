057d287cac412dd92178a76e2598e4f34bf0a94d
UmVitorAleatorio <vitorhiper@hotmail.com>
2025-07-09 09:57:17 -0300

    Projeto aprovado no BeeCrowd, uso de métodos para abstração e legibilidade do código

--- 1071/Soma de Impares Consecutivos I/src/main/java/Main.java
+++ 1071/Soma de Impares Consecutivos I/src/main/java/Main.java
@@ -1,5 +1,42 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        int inicio, fim;
 
+        inicio = leitura();
+        fim = leitura();
+        int[] sequencia = imparesConsecutivos(inicio, fim);
+        System.out.println(soma(sequencia));
+    }
+
+    static Scanner sc = new Scanner(System.in);
+
+    public static int leitura() {
+        return sc.nextInt();
+    }
+
+    public static int[] imparesConsecutivos(int inicio, int fim) {
+        List<Integer> impares = new ArrayList<Integer>();
+
+        if (inicio > fim) {
+            int aux = fim;
+            fim = inicio;
+            inicio = aux;
+        }
+
+        for (int i = inicio + 1; i < fim; i++) {
+            if (i % 2 != 0) {
+                impares.add(i);
+            }
+        }
+        return impares.stream().mapToInt(Integer::intValue).toArray();
+    }
+
+    public static int soma(int[] sequencia) {
+        return Arrays.stream(sequencia).sum();
     }
 }
