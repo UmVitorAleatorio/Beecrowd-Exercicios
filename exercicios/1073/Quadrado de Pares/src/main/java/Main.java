aa93cd871e4ada8115f9bc2ea305581edfe3252b
UmVitorAleatorio <vitorhiper@hotmail.com>
2025-07-10 11:53:55 -0300

    Criando o método para imprimir apenas as raizes quadradas perfeitas

--- exercicios/1073/Quadrado de Pares/src/main/java/Main.java
+++ exercicios/1073/Quadrado de Pares/src/main/java/Main.java
@@ -3,10 +3,17 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         int quantidade = leitura();
+        raizPares(quantidade);
     }
 
-    public static int leitura(){
+    public static int leitura() {
         Scanner sc = new Scanner(System.in);
         return sc.nextInt();
+    }
+
+    public static void raizPares(int quantidade) {
+        for (int i = 2; i <= quantidade; i += 2) {
+            System.out.println(i+"^"+2+" = "+(int)Math.pow(i, 2));
+        }
     }
 }
