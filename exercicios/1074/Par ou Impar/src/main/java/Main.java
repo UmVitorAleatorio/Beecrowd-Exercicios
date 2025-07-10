1d95b95c0e9d7c82ca612744ec5d5ea665818a8a
UmVitorAleatorio <vitorhiper@hotmail.com>
2025-07-10 12:55:38 -0300

    Projeto aprovado no BeeCrowd, Criação de métodos para verificação de par, nulo ou impar, e metodos para enviar a saida ja informando se é positivo, nulo ou negativo

--- exercicios/1074/Par ou Impar/src/main/java/Main.java
+++ exercicios/1074/Par ou Impar/src/main/java/Main.java
@@ -1,5 +1,53 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        int quantidade = leitura();
+        int[] seqNumeros = sequencia(quantidade);
+        verificador(seqNumeros);
+    }
 
+    static Scanner sc = new Scanner(System.in);
+
+    private static int leitura() {
+        return sc.nextInt();
+    }
+
+    private static int[] sequencia(int quantidade) {
+        List<Integer> sequencia = new ArrayList<>();
+        for (int i = 0; i < quantidade; i++) {
+            sequencia.add(sc.nextInt());
+        }
+        return sequencia.stream().mapToInt(Integer::intValue).toArray();
+    }
+
+    private static void verificador(int[] seqNumeros) {
+        for (int i = 0; i < seqNumeros.length; i++) {
+            if (seqNumeros[i] % 2 == 0 && seqNumeros[i] != 0) {
+                pares(seqNumeros, i);
+            } else if (seqNumeros[i] % 2 != 0) {
+                impares(seqNumeros, i);
+            } else {
+                System.out.println("NULL");
+            }
+        }
+    }
+
+    private static void pares(int[] seqNumeros, int i) {
+        if (seqNumeros[i] > 0) {
+            System.out.println("EVEN POSITIVE");
+        } else {
+            System.out.println("EVEN NEGATIVE");
+        }
+    }
+
+    private static void impares(int[] seqNumeros, int i) {
+        if (seqNumeros[i] > 0) {
+            System.out.println("ODD POSITIVE");
+        } else {
+            System.out.println("ODD NEGATIVE");
+        }
     }
 }
