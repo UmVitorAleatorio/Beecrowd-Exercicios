cde46d2e1146c396ebc613f6f2b9152bb5edf63a
UmVitorAleatorio <vitorhiper@hotmail.com>
2025-07-10 11:13:51 -0300

    Criando o metodo leitura

--- exercicios/1073/Quadrado de Pares/src/main/java/Main.java
+++ exercicios/1073/Quadrado de Pares/src/main/java/Main.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        int quantidade = leitura();
+    }
 
+    public static int leitura(){
+        Scanner sc = new Scanner(System.in);
+        return sc.nextInt();
     }
 }
