2786d6452fd10253da23dd1f597e108e5ed72175
UmVitorAleatorio <vitorhiper@hotmail.com>
2025-07-12 17:06:50 -0300

    Projeto aprovado no BeeCrowd, Criação de métodos leitura, e impressão de todos os números divisiveis por n(entrada)

--- exercicios/1075/Resto 2/src/main/java/Main.java
+++ exercicios/1075/Resto 2/src/main/java/Main.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        int divisor = leitor();
+        resto(divisor);
+    }
 
+    private static int leitor() {
+        Scanner sc = new Scanner(System.in);
+        return sc.nextInt();
+    }
+
+    private static void resto(int divisor) {
+        if (divisor == 0) {
+            return;
+        }
+        for (int i = 1; i <= 10000; i++) {
+            if (i % divisor == 2) {
+                System.out.println(i);
+            }
+        }
     }
 }
