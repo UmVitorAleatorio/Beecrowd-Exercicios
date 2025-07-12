d9f0ccbb629f7b41ac72c4b161f144dabf3795b2
UmVitorAleatorio <vitorhiper@hotmail.com>
2025-07-12 18:45:39 -0300

    Projeto aprovado no BeeCrowd, Criação de métodos leitura para coleta de casos, para coleta de notas, e impressão da media ponderada das notas coletadas por caso

--- exercicios/1079/Medias Ponderadas/src/main/java/Main.java
+++ exercicios/1079/Medias Ponderadas/src/main/java/Main.java
@@ -1,5 +1,35 @@
-public class Main {
-    public static void main(String[] args) {
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
+public class Main {
+    static Scanner sc = new Scanner(System.in);
+    static DecimalFormat df = new DecimalFormat("0.0");
+
+    public static void main(String[] args) {
+        int casos = leitor();
+        for (int i = 0; i < casos; i++) {
+            double[] notas = iterador();
+            imprimeMedia(notas);
+        }
+        sc.close();
+    }
+
+    private static int leitor() {
+        return sc.nextInt();
+    }
+
+    private static double[] iterador() {
+        List<Double> lista = new ArrayList<>();
+        for (int j = 0; j < 3; j++) {
+            lista.add(sc.nextDouble());
+        }
+        return lista.stream().mapToDouble(Double::doubleValue).toArray();
+    }
+
+    private static void imprimeMedia(double[] notas) {
+        double media = ((notas[0] * 2) + (notas[1] * 3) + (notas[2] * 5)) / 10;
+        System.out.println(df.format(media));
     }
 }
