6cca61cd55862ad64663996d18e486be98144933
UmVitorAleatorio <vitorhiper@hotmail.com>
2025-07-12 17:20:35 -0300

    Projeto aprovado no BeeCrowd, Criação de métodos leitura, e impressão da tabuadas de n(entrada)

--- exercicios/1078/Tabuada/src/main/java/Main.java
+++ exercicios/1078/Tabuada/src/main/java/Main.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        int multiplicador = leitor();
+        imprime(multiplicador);
+    }
 
+    private static int leitor() {
+        Scanner sc = new Scanner(System.in);
+        return sc.nextInt();
+    }
+
+    private static void imprime(int multiplicador) {
+        for (int i = 1; i < 11; i++) {
+            System.out.println(i + " x " + multiplicador + " = " + (multiplicador * i));
+        }
     }
 }
