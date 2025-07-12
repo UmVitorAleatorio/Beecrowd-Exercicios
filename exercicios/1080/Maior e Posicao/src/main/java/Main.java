5afdaf6579771fbe5a9c3ed4e6b5fe4592bc92b5
UmVitorAleatorio <vitorhiper@hotmail.com>
2025-07-12 19:11:43 -0300

    Projeto aprovado no BeeCrowd, Criação do método leitura e por fim gravação e impressão do maior número e sua posição dentre os 100 números analisados

--- exercicios/1080/Maior e Posicao/src/main/java/Main.java
+++ exercicios/1080/Maior e Posicao/src/main/java/Main.java
@@ -1,5 +1,24 @@
-public class Main {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class Main {
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int[] maior = new int[2];
+        int aux;
+
+        for (int i = 0; i < 100; i++) {
+            aux = leitura();
+            if (aux > maior[0]) {
+                maior[0] = aux;
+                maior[1] = i+1;
+            }
+        }
+
+        System.out.println(maior[0] +"\n"+ maior[1]);
+    }
+
+    private static int leitura() {
+        return sc.nextInt();
     }
 }
