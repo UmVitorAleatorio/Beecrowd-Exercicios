fdde380cde3b8c34152c237fd781bef431087b12
UmVitorAleatorio <vitorhiper@hotmail.com>
2025-07-16 17:15:47 -0300

    Projeto aprovado no BeeCrowd

--- exercicios/1097/Sequencia IJ 3/src/main/java/Main.java
+++ exercicios/1097/Sequencia IJ 3/src/main/java/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-
+        int aux = 7;
+        for (int i = 1; i <= 9; i += 2) {
+            for (int j = aux; j != aux-3; j--){
+                System.out.println("I="+i+" J="+j);
+            }
+            aux += 2;
+        }
     }
 }
