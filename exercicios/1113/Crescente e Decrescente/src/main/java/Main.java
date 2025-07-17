465156609fb4d40c30dc3e1968e0537fd868aa49
UmVitorAleatorio <vitorhiper@hotmail.com>
2025-07-17 18:04:11 -0300

    Projeto aprovado no BeeCrowd

--- exercicios/1113/Crescente e Decrescente/src/main/java/Main.java
+++ exercicios/1113/Crescente e Decrescente/src/main/java/Main.java
@@ -1,5 +1,31 @@
-public class Main {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class Main {
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        do {
+            int n1 = leitor();
+            int n2 = leitor();
+
+            if (n1 == n2) {
+                break;
+            }
+
+            verifica(n1, n2);
+        } while (true);
+        sc.close();
+    }
+
+    private static int leitor() {
+        return sc.nextInt();
+    }
+
+    private static void verifica(int n1, int n2) {
+        if (n1 < n2) {
+            System.out.println("Crescente");
+        }else {
+            System.out.println("Decrescente");
+        }
     }
 }
