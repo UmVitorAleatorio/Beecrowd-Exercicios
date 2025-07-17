3f9e63a982c01b806f8350dff182fe96823fc6bc
UmVitorAleatorio <vitorhiper@hotmail.com>
2025-07-17 18:34:42 -0300

    Projeto aprovado no BeeCrowd

--- exercicios/1114/Senha Fixa/src/main/java/Main.java
+++ exercicios/1114/Senha Fixa/src/main/java/Main.java
@@ -1,5 +1,29 @@
-public class Main {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class Main {
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        do {
+            String tentativa = leitor();
+            if (verifica(tentativa)){
+                break;
+            }
+        } while (true);
+        sc.close();
+    }
+
+    private static String leitor() {
+        return sc.nextLine();
+    }
+
+    private static boolean verifica(String tentativa) {
+        String senhaCorreta = "2002";
+        if (tentativa.equals(senhaCorreta)) {
+            System.out.println("Acesso Permitido");
+            return true;
+        }
+        System.out.println("Senha Invalida");
+        return false;
     }
 }
