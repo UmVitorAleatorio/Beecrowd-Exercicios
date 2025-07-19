e994c18948ca2b7ae625373685631738fdaa2ad9
UmVitorAleatorio <vitorhiper@hotmail.com>
2025-07-19 08:57:15 -0300

    Projeto aprovado no BeeCrowd

--- exercicios/1115/Quadrante/src/main/java/Main.java
+++ exercicios/1115/Quadrante/src/main/java/Main.java
@@ -1,5 +1,33 @@
-public class Main {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class Main {
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        do {
+            int x = leitura();
+            int y = leitura();
+            if (x == 0 || y == 0) {
+                break;
+            }
+            verificaQuadrante(x,y);
+        }while (true);
+        sc.close();
+    }
+
+    private static int leitura() {
+        return sc.nextInt();
+    }
+
+    private static void verificaQuadrante(int x, int y) {
+        if (x > 0 && y > 0){
+            System.out.println("primeiro");
+        }else if (x < 0 && y > 0){
+            System.out.println("segundo");
+        }else if (x < 0 && y < 0){
+            System.out.println("terceiro");
+        }else {
+            System.out.println("quarto");
+        }
     }
 }
