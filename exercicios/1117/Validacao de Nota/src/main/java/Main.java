91e291628f7bba65845eec9a10f23b744460d407
UmVitorAleatorio <vitorhiper@hotmail.com>
2025-07-19 11:25:14 -0300

    Projeto aprovado no BeeCrowd

--- exercicios/1117/Validacao de Nota/src/main/java/Main.java
+++ exercicios/1117/Validacao de Nota/src/main/java/Main.java
@@ -1,5 +1,36 @@
-public class Main {
-    public static void main(String[] args) {
+import java.text.DecimalFormat;
+import java.util.Scanner;
 
+public class Main {
+    static Scanner sc = new Scanner(System.in);
+    static DecimalFormat df = new DecimalFormat("0.00");
+
+    public static void main(String[] args) {
+        double nota1 = leitura();
+        while (!isEntreZeroEDez(nota1)) {
+            nota1 = leitura();
+        }
+        double nota2 = leitura();
+        while (!isEntreZeroEDez(nota2)) {
+            nota2 = leitura();
+        }
+        imprimeNota(nota1, nota2);
+        sc.close();
+    }
+
+    private static double leitura() {
+        return sc.nextDouble();
+    }
+
+    private static boolean isEntreZeroEDez(double nota) {
+        if (nota < 0 || nota > 10) {
+            System.out.println("nota invalida");
+            return false;
+        }
+        return true;
+    }
+
+    private static void imprimeNota(double nota1, double nota2) {
+        System.out.println("media = " + df.format((nota1 + nota2) / 2));
     }
 }
