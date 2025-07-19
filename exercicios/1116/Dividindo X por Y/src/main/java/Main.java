b8ee19424991d6b20742bb26f68086e6294f2382
UmVitorAleatorio <vitorhiper@hotmail.com>
2025-07-19 09:47:42 -0300

    Projeto aprovado no BeeCrowd

--- exercicios/1116/Dividindo X por Y/src/main/java/Main.java
+++ exercicios/1116/Dividindo X por Y/src/main/java/Main.java
@@ -1,5 +1,29 @@
-public class Main {
-    public static void main(String[] args) {
+import java.text.DecimalFormat;
+import java.util.Scanner;
 
+public class Main {
+    static Scanner sc = new Scanner(System.in);
+    static DecimalFormat df = new DecimalFormat("0.0");
+
+    public static void main(String[] args) {
+        int aux = leitura();
+        for (int i = 0; i < aux; i++) {
+            int x = leitura();
+            int y = leitura();
+            divisaoXY(x,y);
+        }
+        sc.close();
+    }
+
+    private static int leitura() {
+        return sc.nextInt();
+    }
+
+    private static void divisaoXY(int x, double y) {
+        if (y == 0) {
+            System.out.println("divisao impossivel");
+            return;
+        }
+        System.out.println(df.format(x/y));
     }
 }
