a9d19d8d1007aa4f511d76a91bc0cf3e74f42321
UmVitorAleatorio <vitorhiper@hotmail.com>
2025-07-26 20:52:39 -0300

    Projeto aprovado no BeeCrowd

--- exercicios/1133/Resto da Divisao/src/main/java/Main.java
+++ exercicios/1133/Resto da Divisao/src/main/java/Main.java
@@ -1,5 +1,27 @@
-public class Main {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class Main {
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int entrada = leitura();
+        int saida = leitura();
+        imprimeResto(entrada, saida);
+        sc.close();
+    }
+
+    private static int leitura() {
+        return sc.nextInt();
+    }
+
+    private static void imprimeResto(int entrada, int saida) {
+        int menor = Math.min(entrada, saida);
+        int maior = Math.max(entrada, saida);
+
+        for (int i = menor+1; i < maior; i++) {
+            if (i % 5 == 2 || i % 5 == 3) {
+                System.out.println(i);
+            }
+        }
     }
 }
