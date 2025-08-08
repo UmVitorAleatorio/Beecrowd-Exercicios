2b35050dc9de44267cd862d5ead8c508565c7f16
UmVitorAleatorio <vitorhiper@hotmail.com>
2025-08-07 21:39:52 -0300

    Projeto aprovado no BeeCrowd

--- exercicios/1134/Tipo de Combustivel/src/main/java/Main.java
+++ exercicios/1134/Tipo de Combustivel/src/main/java/Main.java
@@ -1,5 +1,39 @@
-public class Main {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class Main {
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int alcool = 0, gasolina = 0, disel = 0;
+        boolean continuar = true;
+
+        while (continuar) {
+            int valorSwitch = leitura();
+            switch (valorSwitch) {
+                case 1:
+                    alcool++;
+                    break;
+                case 2:
+                    gasolina++;
+                    break;
+                case 3:
+                    disel++;
+                    break;
+                case 4:
+                    continuar = false;
+                    break;
+                default:
+                    break;
+            }
+        }
+        System.out.println("MUITO OBRIGADO\n" +
+                "Alcool: "+alcool+"\n" +
+                "Gasolina: "+gasolina+"\n" +
+                "Diesel: "+disel);
+        sc.close();
+    }
+
+    private static int leitura() {
+        return sc.nextInt();
     }
 }
